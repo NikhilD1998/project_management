b8767a861fa2538a49bfa512f800683a61c73f19
NikhilD1998 <nikhildadab@hotmail.com>
2024-10-24 17:20:38 +0530

    Showing Add Project form Dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected.Jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NoProjectSelected />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
